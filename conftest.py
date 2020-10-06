1f0ea6cfccb74d5f417fe03c3177bb91c8b1434d
Chilopy <ktmironova@gmail.com>
2020-10-06 21:16:23 +0400

    first test main page

--- conftest.py
+++ conftest.py
@@ -2,15 +2,15 @@ import pytest
 from selenium import webdriver
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
+    parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
-    parser.addoption('--user_language', action='store', default=en,
+    parser.addoption('--language', action='store', default="en",
                      help="Choose language: ru, en, ...")
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
-    user_language = request.config.getoption("user_language")
+    user_language = request.config.getoption("language")
     browser = None
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
