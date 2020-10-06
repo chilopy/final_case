fe61ec31eb55ecd49152cfbbb9e2a686adefca4e
Chilopy <ktmironova@gmail.com>
2020-10-06 20:26:29 +0400

    added conftest.py

---
+++ conftest.py
@@ -0,0 +1,31 @@
+import pytest
+from selenium import webdriver
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--user_language', action='store', default=None,
+                     help="Choose language: ru, en, ...")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("user_language")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        chrome_options.add_argument("--remote-debugging-port=9222")  
+        browser = webdriver.Chrome(options=chrome_options)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+   
